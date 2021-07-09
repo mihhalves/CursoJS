0a0b05c3344d2f359ff9e9c8e1ec0badb57352d4
mihhalves <michelle.selva@gmail.com>
2021-07-09 16:29:20 -0300

    JavaScrpit

---
+++ ex014/Aula 12/ex011.js
@@ -0,0 +1,9 @@
+var idade =  80
+
+if (idade < 16 || idade > 65) {
+    console.log("Voto opcional")
+} else if (idade <18) {
+    console.log("não vota")
+} else {
+    console.log("Voto obrigatorio")
+}
---
+++ ex014/Aula 15/ex16.js
@@ -0,0 +1,7 @@
+let valores = [1,2,3,4,5]
+
+for(let pos in valores) {
+    console.log(`A posicao ${pos} tem o valor ${valores[pos]}`)
+}
+
+
